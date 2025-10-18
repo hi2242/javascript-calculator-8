8b6146e3b48cf08c83df5de259254482d5cf658d
hi2242 <doodukly1999@gmail.com>
2025-10-19 02:00:54 +0900

    test : 콜론(:) 구분자 지원 기능 임시 테스트 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import add from "./StringCalculator.js";
 class App {
-  async run() {}
+  async run() {
+    console.log("--- 덧셈 계산기 테스트 시작 ---");
+
+    console.log(`입력: "1,2:3" => 결과: ${add("1,2:3")}`); // 예상: 6
+
+    console.log("--- 덧셈 계산기 테스트 종료 ---");
+  }
 }
 
 export default App;
