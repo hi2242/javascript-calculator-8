951d7448fc3c9ace65075e9f169fef6b7eb4bbb4
hi2242 <doodukly1999@gmail.com>
2025-10-19 01:41:33 +0900

    feat : 쉼표(,) 구분자를 사용한 기본 덧셈 기능 구현

---
+++ src/StringCalculator.js
@@ -0,0 +1,16 @@
+function add(text) {
+  // 입력값이 null이거나 빈 문자열이면 0을 반환
+  if (!text) {
+    return 0;
+  }
+
+  // 쉼표(,)를 기준으로 문자열을 분리하여 숫자 배열로 만듦
+  const numbers = text.split(',').map(Number);
+  
+  // 배열의 모든 숫자를 더함
+  const sum = numbers.reduce((acc, current) => acc + current, 0);
+
+  return sum;
+}
+
+export default add;
