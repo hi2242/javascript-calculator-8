3037ba88df8e4d20a9d760fe2d4aa7d7e0be69f0
hi2242 <doodukly1999@gmail.com>
2025-10-19 14:10:27 +0900

    feat : 사용자 입출력 인터페이스 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import add from "./StringCalculator.js";
 class App {
-  async run() {}
+  async run() {
+    MissionUtils.Console.print("덧셈할 문자열을 입력해 주세요.");
+    const input = await MissionUtils.Console.readLineAsync("");
+
+    try {
+      const result = add(input);
+      MissionUtils.Console.print(`결과 : ${result}`);
+    } catch (error) {
+      throw error;
+    }
+  }
 }
 
 export default App;
