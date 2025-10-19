58e32820e91027ef43b37d719b5d0d5704613359
hi2242 <doodukly1999@gmail.com>
2025-10-19 12:20:03 +0900

    feat : 커스텀 구분자 파싱 및 게산 기능 구현

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -4,13 +4,31 @@ function add(text) {
     return 0;
   }
 
-  // 쉼표(,) 또는 콜론(:)을 구분자로 사용하도록 정규표현식으로 변경
-  const numbers = text.split(/[,:]/).map(Number);
+  let delimiters = /[,:]/;
+  let numbersText = text;
+
+  // '//'로 시작한다면 커스텀 구분자 찾기
+  if (text.startsWith("//")) {
+    // 첫 번째 줄바꿈(\n)을 기준으로 구분자 라인과 숫자 문자열 라인을 분리
+    const PARTS = text.split("\n");
+    const DELIMITER_LINE = PARTS[0];
+    // 두 번째 부분부터 숫자 문자열
+    numbersText = PARTS[1];
+
+    // '//'를 제외하여 커스텀 구분자 추출
+    const CUSTOM_DELIMITER = DELIMITER_LINE.substring(2);
+
+    // 기존 구분자와 커스텀 구분자를 모두 포함하는 새 정규표현식을 생성
+    delimiters = new RegExp(`[,:${CUSTOM_DELIMITER}]`);
+  }
+  console.log(`구분자 : ${delimiters}`);
+  // 앞에서 정해진 구분자를 통해 숫자 분리
+  const NUMBERS = numbersText.split(delimiters).map(Number);
 
   // 배열의 모든 숫자를 더함
-  const sum = numbers.reduce((acc, current) => acc + current, 0);
+  const SUM = NUMBERS.reduce((acc, current) => acc + current, 0);
 
-  return sum;
+  return SUM;
 }
 
 export default add;
